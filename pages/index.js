f2fbff74f1c558293eda4b8f4bbcbb2c2cea4401
Shaun <shaunsanmiguel@gmail.com>
2022-08-27 22:13:25 +0800

    2022 project

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>2022 Project</h1>
+      <h3>Enrollment App</h3>
+    </div>
+  )
+}
