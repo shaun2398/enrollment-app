24e5743d6d75030a41ba4df94a7ff3415eac5b67
Shaun <shaunsanmiguel@gmail.com>
2022-09-02 00:39:02 +0800

    add layout page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
+//React
 import React from "react"
+
+// Components
+
+
+//Styles
+import styles from '../styles/Home.module.css'
+
+
 
 
 export default function Home() {
 
   return (
-    <div>
-      <h1 className="is-size-1">Hello Next</h1>
-    </div>
-    
+      <div className={styles.container}>
+        <h1 className="is-size-1 is-capitalized has-text-centered">hello next</h1>
+      </div>
   )
 }
