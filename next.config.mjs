e5cbe66a73f1ee123e88137f61d5fdcbbc2bfd35
Shaun <shaunsanmiguel@gmail.com>
2022-10-26 21:22:20 +0800

    updated next config and package json

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /**
    * @type {import('next').NextConfig}
    */
- const nextConfig   = {
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
-};
+ const nextConfig = {
+   images: {
+     loader: "akamai",
+     path: "",
+   },
+   assetPrefix: "./",
+ };
 
 export default nextConfig;
 
