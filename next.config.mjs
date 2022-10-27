429f7d99487d156852b0eb52445306d537970ceb
Shaun <shaunsanmiguel@gmail.com>
2022-10-27 21:07:28 +0800

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  assetPrefix: "./",
+  basePath: "./pages",
+  assetPrefix: "./pages/",
 };
 
-export default   nextConfig;
+export default nextConfig;
